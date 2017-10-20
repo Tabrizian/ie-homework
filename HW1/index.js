1e841cffe9c5edfd7f53b545a5b78105de00ece1
Iman Tabrizian <tabrizian@outlook.com>
2017-10-20 21:13:02 +0330

    job is done

--- HW1/index.js
+++ HW1/index.js
@@ -1,18 +1,50 @@
-const axios = require('axios')
 const chalk = require('chalk')
+const http = require('http')
 const readline = require('readline')
+const cookie = require('cookie')
+const prettyjson = require('prettyjson')
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 })
 
-console.log(chalk.blue.bgRed.bold('Enter the url to analyze:'));
+rl.question(chalk.blue.bgRed.bold('Enter the url to analyze:'), (url) => {
+  rl.question(chalk.blue.bgRed.bold('Persistent? (true|false)'), (persistent) => {
+    const agent = new http.Agent({ keepAlive: persistent === 'true' ? true : false});
+    let parts = url.split('/')
+    let hostname = parts[0]
+    parts.splice(0, 1)
+    let path = '/' + parts.join('/')
+    let req = http.request({
+      method: 'OPTIONS',
+      hostname: hostname,
+      port: 80,
+      path: path,
+      agent: agent
+    }, (res) => {
+      console.log(chalk.blue('Allowed Methods: ') + chalk.green.bold(res.headers.allow))
+    })
+    req.end()
 
-rl.on('line', async (url) => {
-  let response = await axios.options(url)
-  console.log(response.cookies)
-  console.log('Allowed Methods: ' + chalk.green.bold(response.headers.allow))
-  rl.close()
+    req = http.request({
+      method: 'GET',
+      hostname: hostname,
+      port: 80,
+      path: path,
+      agent: agent
+    }, (res) => {
+      let cookies = []
+      console.log(chalk.blue('Cookies'))
+      for (let cookieItem of res.headers['set-cookie']) {
+        let parsed = cookie.parse(cookieItem)
+        cookies.push(parsed)
+      }
+      console.log(prettyjson.render(cookies))
+      console.log(chalk.blue('Authentication: ') + chalk.green.bold(res.headers['www-authenticate'] || 'No authentication'))
+    })
+    req.end()
+  })
+
 })
 
