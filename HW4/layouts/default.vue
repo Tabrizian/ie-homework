41e9d6b82e74e409d31404ed254bfabdec27bf33
Iman Tabrizian <Tabrizian@outlook.com>
2018-01-19 20:15:02 +0330

    Some improvements

--- HW4/layouts/default.vue
+++ HW4/layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
+
   <div>
+    <div class="header">
+      <button type="button">ورود / عضویت</button>
+      <i class="fa fa-save fa-lg" aria-hidden="true"></i>
+      <div class="search">
+        <input type="text" placeholder="جست‌و‌جوی فیلم، سریال، بازیگر، کارگردان">
+        <i class="fa fa-search fa-lg" aria-hidden="true"></i>
+      </div>
+    </div>
+
     <nuxt/>
   </div>
 </template>
