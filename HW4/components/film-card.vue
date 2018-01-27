2d68dbc0f4c6a00267232c0e1a60c8f3135a0dca
Iman Tabrizian <tabrizian@outlook.com>
2018-01-28 00:34:12 +0330

    Add film-card component

---
+++ HW4/components/film-card.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="root" :style="{background: colorTop}">
+        <img :src="image">
+        <div class="row">
+            <div class="col tag">
+                Salam
+            </div>
+            <div class="col tag">
+                Salam
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.root {
+    height: 300px;
+    width: 200px;
+    border: 1px solid black;
+    background-color: blue;
+    padding: 15px;
+}
+
+img {
+    height: 230px;
+    margin-bottom: 5px;
+}
+
+.tag  {
+    border: 1px solid black;
+    padding: 5px;
+    margin-left: 5px;
+    border-radius: 5px;
+    background-color: white;
+    display: inline-block;
+    text-align: center;
+}
+
+
+</style>
+
+<script>
+export default {
+    props: {
+        image: {
+            type: String
+        },
+        rating: {
+            type: String
+        },
+        tags: {
+            type: Array
+        },
+        quality: {
+            type: String
+        },
+        rating: {
+            type: String
+        },
+        colorTop: {
+            type: String
+        },
+        colorBottom: {
+            type: String
+        }
+
+    }
+}
+</script>
--- HW4/pages/index.vue
+++ HW4/pages/index.vue
@@ -1,71 +1,71 @@
 <template>
   <div>
-  <div class="root-element">
-    <div class="title">
-      دانلود، فیلم، سریال، انیمیشن، مستند و دوبله با ۱۳,۶۲۱ عنوان 
+    <div class="root-element">
+      <div class="title">
+        دانلود، فیلم، سریال، انیمیشن، مستند و دوبله با ۱۳,۶۲۱ عنوان 
+      </div>
+
+      <search-icon  class="search-icon" icon="search" color="purple"></search-icon>
+      <search-icon  class="search-icon" icon="search-plus" ></search-icon>
+      <search-input class="search-input" hint="جست‌و‌جوی، فیلم، سریال بازیگر و کارگردان"></search-input>
+      <m-button  class="search" color="blue">
+        جست و جو
+      </m-button>
     </div>
-
-    <search-icon  class="search-icon" icon="search" color="purple"></search-icon>
-    <search-icon  class="search-icon" icon="search-plus" ></search-icon>
-    <search-input class="search-input" hint="جست‌و‌جوی، فیلم، سریال بازیگر و کارگردان"></search-input>
-    <m-button  class="search" color="blue">
-      جست و جو
-    </m-button>
-  </div>
   <div class="movies">
-  <span class="movies-title">جدیدترین‌ها</span>
-
-        <div class="movies-category">
-            <ul>
-                <li class="active">همه</li>
-                <li>فیلم سینمایی</li>
-                <li>انیمیشن</li>
-                <li>مستند</li>
-            </ul>
+    <span class="movies-title">جدیدترین‌ها</span>
+    <div class="movies-category">
+      <ul>
+        <li class="active">همه</li>
+        <li>فیلم سینمایی</li>
+        <li>انیمیشن</li>
+        <li>مستند</li>
+      </ul>
+    </div>
+    <div style="clear: both;"></div>
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide" name="tt0418279">
+          <a href="./download.html?movie=tt0418279"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BNDg1NTU2OWEtM2UzYi00ZWRmLWEwMTktZWNjYWQ1NWM1OThjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
         </div>
-        <div style="clear: both;"></div>
-  <div class="swiper-container">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide" name="tt0418279">
-                    <a href="./download.html?movie=tt0418279"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BNDg1NTU2OWEtM2UzYi00ZWRmLWEwMTktZWNjYWQ1NWM1OThjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
-                </div>
-                <div class="swiper-slide" name="tt1707386">
-                    <a href="./download.html?movie=tt1707386"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ4NDI3NDg4M15BMl5BanBnXkFtZTcwMjY5OTI1OA@@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
-                </div>
-                <div class="swiper-slide" name="tt5721088">
-                    <a href="./download.html?movie=tt5721088"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMzk0NDdjNWEtMDVkMC00ODdiLTgyMmYtOWM0ZDRjOTU4MjJlXkEyXkFqcGdeQXVyNjM2MDMxMDk@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
-                </div>
-                <div class="swiper-slide" name="tt3521126">
-                    <a href="./download.html?movie=tt3521126"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BOGNkMzliMGMtMDI5Ni00OTZkLTgyMTYtNzk5ZTY1NjVhYjVmXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
-                </div>
-                <div class="swiper-slide" name="tt4864624">
-                    <a href="./download.html?movie=tt4864624"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDQ3MTAyZjMtZjVkMC00NzFmLWE2ZjYtZDkyYzlmZWU4NTAwXkEyXkFqcGdeQXVyMTg1MzYyMzQ@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
-                </div>
-                <div class="swiper-slide" name="tt3266284">
-                    <a href="./download.html?movie=tt3266284"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMzQwMzQ0MjYzMV5BMl5BanBnXkFtZTgwOTIwMTEwNDI@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
-                </div>
-                <div class="swiper-slide" name="tt5657846">
-                    <a href="./download.html?movie=tt5657846"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BNDA4Nzc1OTg2OV5BMl5BanBnXkFtZTgwODE3ODgwNDI@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
-                </div>
-                <div class="swiper-slide" name="tt3402236">
-                    <a href="./download.html?movie=tt3402236"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BNGFmM2NmYjYtMjAwNy00ZDkzLWI3ZWMtOGZhOTRhYzQwMTA0XkEyXkFqcGdeQXVyNzU2MzMyNTI@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
-                </div>
-                <div class="swiper-slide" name="tt5027774">
-                    <a href="./download.html?movie=tt5027774"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjMxNzgwMDUyMl5BMl5BanBnXkFtZTgwMTQ0NTIyNDM@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
-                </div>
-            </div>
-            <!-- Add Pagination -->
-            <div class="swiper-button-prev"></div>
-            <div class="swiper-button-next"></div>
-  </div>
+        <div class="swiper-slide" name="tt1707386">
+          <a href="./download.html?movie=tt1707386"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ4NDI3NDg4M15BMl5BanBnXkFtZTcwMjY5OTI1OA@@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
+        </div>
+        <div class="swiper-slide" name="tt5721088">
+          <a href="./download.html?movie=tt5721088"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMzk0NDdjNWEtMDVkMC00ODdiLTgyMmYtOWM0ZDRjOTU4MjJlXkEyXkFqcGdeQXVyNjM2MDMxMDk@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
+        </div>
+        <div class="swiper-slide" name="tt3521126">
+          <a href="./download.html?movie=tt3521126"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BOGNkMzliMGMtMDI5Ni00OTZkLTgyMTYtNzk5ZTY1NjVhYjVmXkEyXkFqcGdeQXVyNTAzMTY4MDA@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
+        </div>
+        <div class="swiper-slide" name="tt4864624">
+          <a href="./download.html?movie=tt4864624"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMDQ3MTAyZjMtZjVkMC00NzFmLWE2ZjYtZDkyYzlmZWU4NTAwXkEyXkFqcGdeQXVyMTg1MzYyMzQ@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
+        </div>
+        <div class="swiper-slide" name="tt3266284">
+          <a href="./download.html?movie=tt3266284"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMzQwMzQ0MjYzMV5BMl5BanBnXkFtZTgwOTIwMTEwNDI@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
+        </div>
+        <div class="swiper-slide" name="tt5657846">
+          <a href="./download.html?movie=tt5657846"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BNDA4Nzc1OTg2OV5BMl5BanBnXkFtZTgwODE3ODgwNDI@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
+        </div>
+        <div class="swiper-slide" name="tt3402236">
+          <a href="./download.html?movie=tt3402236"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BNGFmM2NmYjYtMjAwNy00ZDkzLWI3ZWMtOGZhOTRhYzQwMTA0XkEyXkFqcGdeQXVyNzU2MzMyNTI@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
+        </div>
+        <div class="swiper-slide" name="tt5027774">
+          <a href="./download.html?movie=tt5027774"><img src="https://images-na.ssl-images-amazon.com/images/M/MV5BMjMxNzgwMDUyMl5BMl5BanBnXkFtZTgwMTQ0NTIyNDM@._V1_UX182_CR0,0,182,268_AL_.jpg"></a>
+        </div>
+      </div>
+              <!-- Add Pagination -->
+    <div class="swiper-button-prev"></div>
+    <div class="swiper-button-next"></div>
+    </div>
   </div>
   <static-banner image="/images/static-banner.png" color="#ab0606" title="اسطوره‌ی کنگ‌فو و رویای صعود به قله!" subtitle="از بهترین نبردهای بروسلی">
     <m-button color="Transparent">
       طرفدارای بروسلی دانلود کنن!
     </m-button>
   </static-banner>
+  <film-card colorTop="purple" image="https://images-na.ssl-images-amazon.com/images/M/MV5BMjMxNzgwMDUyMl5BMl5BanBnXkFtZTgwMTQ0NTIyNDM@._V1_UX182_CR0,0,182,268_AL_.jpg">
+  </film-card>
   </div>
-
 </template>
 
 <style scoped>
@@ -96,7 +96,6 @@
 .search-icon {
   margin-left: 10px;
 }
-
 
 .movies {
     background-color: black;
@@ -135,10 +134,6 @@
     border-radius: 5px;
 
 }
-/* img { position: relative; */
-  /* top: -160px; */
-  /* left: -900px; */
-/* } */
 
 </style>
 
@@ -147,7 +142,7 @@ import SearchInput from "~/components/search-input"
 import SearchIcon from "~/components/search-icon"
 import StaticBanner from "~/components/static-banner"
 import MButton from "~/components/m-button"
-
+import FilmCard from "~/components/film-card"
 
 export default {
   data() {
@@ -181,7 +176,8 @@ export default {
     SearchInput,
     SearchIcon,
     StaticBanner,
-    MButton
+    MButton,
+    FilmCard
   }
   
 }
