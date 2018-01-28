4b40b3394be38fa266ac154d1d2583d3bf2bf419
Iman Tabrizian <Tabrizian@outlook.com>
2018-01-28 20:18:46 +0330

    Add file upload + creating new films

--- HW5/controllers/public/movies.js
+++ HW5/controllers/public/movies.js
@@ -1,20 +1,37 @@
 const {Controller} = require('bak')
 const {Movie, Comment} = require('../../models')
 const Boom = require('boom')
+const fs = require('fs')
 
 class MoviesController extends Controller {
   init () {
     this.get('/posters/{poster}', this.getMoviePoster)
-    this.get('/movies/recent/{number}', this.getRecentMovie)
+    this.get('/movies/recent/{number?}', this.getRecentMovie)
     this.get('/movies/{movie}/detail', this.getMovieDetail)
     this.get('/movies/{movie}/comments', this.getMovieComments)
     this.post('/movies/{movie}/comments', this.createMovieComment)
+    this.post('/movies/submit', this.submitNewMovie)
   }
 
   async getMoviePoster (request, h) {
+    let path = `./public/${request.params.poster}`;
+    return h.file(path);
   }
 
   async getRecentMovie (request, h) {
+    let number = request.query.number || request.params.number || null
+    try {
+      let recentMovies = await Movie.find().sort({created_at: -1})
+      if (number) {
+        let movie = recentMovies[number]
+        return movie
+      } else {
+        return recentMovies
+      }
+    } catch (e) {
+      console.log(e)
+      throw Boom.badRequest()
+    }
   }
 
   async getMovieDetail (request, h) {
@@ -51,6 +68,42 @@ class MoviesController extends Controller {
       console.log(e)
       throw Boom.badRequest()
     }
+  }
+
+  async submitNewMovie (request, h) {
+    let movie = new Movie
+    let {title, original_title, rate, year, length,
+      language, country, description, director, poster} = request.payload
+
+    movie.title = title
+    movie.original_title = original_title
+    movie.rate = rate
+    movie.year = year
+    movie.length = length
+    movie.language = language
+    movie.country = country
+    movie.description = description
+    movie.director = director
+
+
+    try {
+      movie = await movie.save()
+      if (request.payload.poster) {
+        if (request.payload.poster instanceof Buffer) {
+          fs.writeFile(`./public/${movie._id}`, request.payload.poster,  "binary",function(err) {
+            if(err) {
+              console.log(err);
+            } else {
+              console.log("The file was saved!");
+            }
+          });
+
+        }
+      }
+    } catch (e) {
+      throw new Boom.badRequest()
+    }
+
   }
 }
 
