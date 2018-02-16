f403dbdcb4b90ffb583d8b1fa041282183496599
Iman Tabrizian <tabrizian@outlook.com>
2018-02-16 12:15:48 +0330

    Minor improvement

--- HW4/components/film-card.vue
+++ HW4/components/film-card.vue
@@ -16,75 +16,73 @@
                 </div>
             </div>
         </div>
-
     </div>
 </template>
 
 <style scoped>
 .root {
-    height: 370px;
-    width: 220px;
-    padding: 5px;
-    display: inline-block;
-    margin: 5px;
-    background-color: blue;
+  height: 370px;
+  width: 220px;
+  padding: 5px;
+  display: inline-block;
+  margin: 5px;
+  background-color: blue;
 }
 
 img {
-    margin: 0 auto;
-    margin-top: 15px;
-    display: block;
-    height: 240px;
-    margin-bottom: 5px;
+  margin: 0 auto;
+  margin-top: 15px;
+  display: block;
+  height: 240px;
+  margin-bottom: 5px;
 }
 
-.tag  {
-    border: 1px solid black;
-    padding: 5px;
-    margin-left: 5px;
-    border-radius: 5px;
-    background-color: white;
-    display: inline-block;
-    text-align: center;
+.tag {
+  border: 1px solid black;
+  padding: 5px;
+  margin-left: 5px;
+  border-radius: 5px;
+  background-color: white;
+  display: inline-block;
+  text-align: center;
 }
 
 .quality {
-    border: 1px solid black;
-    padding: 5px;
-    margin-left: 5px;
-    margin-top: 5px;
-    border-radius: 5px;
-    background-color: transparent;
-    display: inline-block;
-    text-align: center;
+  border: 1px solid black;
+  padding: 5px;
+  margin-left: 5px;
+  margin-top: 5px;
+  border-radius: 5px;
+  background-color: transparent;
+  display: inline-block;
+  text-align: center;
 }
 </style>
 
 <script>
 export default {
-    props: {
-        image: {
-            type: String
-        },
-        rating: {
-            type: String
-        },
-        tags: {
-            type: Array
-        },
-        quality: {
-            type: String
-        },
-        rating: {
-            type: String
-        },
-        colorTop: {
-            type: String
-        },
-        colorBottom: {
-            type: String
-        }
-
+  props: {
+    image: {
+      type: String
+    },
+    rating: {
+      type: String
+    },
+    tags: {
+      type: Array
+    },
+    quality: {
+      type: String
+    },
+    rating: {
+      type: String
+    },
+    colorTop: {
+      type: String
+    },
+    colorBottom: {
+      type: String
     }
-}
+  }
+};
 </script>
